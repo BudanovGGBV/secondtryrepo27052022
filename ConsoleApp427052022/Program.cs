f769ff653e2f9581d3e597a2565e8d777cafb434
Georgii Budanov <GGBV@simcorp.com>
2022-05-27 10:03:40 +0200

    init commit

---
+++ ConsoleApp427052022/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp427052022
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
